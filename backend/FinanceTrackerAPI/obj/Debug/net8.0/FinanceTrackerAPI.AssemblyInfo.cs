794660317154beb95b10dfad3e0c112fed856052
Stefanos Ugbit <stefanosu422@gmail.com>
2025-07-01 21:36:50 -0400

    updated assembly files

--- backend/FinanceTrackerAPI/obj/Debug/net8.0/FinanceTrackerAPI.AssemblyInfo.cs
+++ backend/FinanceTrackerAPI/obj/Debug/net8.0/FinanceTrackerAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinanceTrackerAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+961b69ccc366c12f397ae2be784feee18111e2c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0942495f31638df7d72399bdd788f846260aac67")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinanceTrackerAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinanceTrackerAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
