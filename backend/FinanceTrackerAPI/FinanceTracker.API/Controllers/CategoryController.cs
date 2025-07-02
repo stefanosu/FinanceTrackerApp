c92de007fc0316c13a902dc35485a27c7b9348ca
Stefanos Ugbit <stefanosu422@gmail.com>
2025-07-02 19:20:49 -0400

    added get and post methods in category controllers

--- backend/FinanceTrackerAPI/FinanceTracker.API/Controllers/CategoryController.cs
+++ backend/FinanceTrackerAPI/FinanceTracker.API/Controllers/CategoryController.cs
@@ -1,8 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using FinanceTrackerAPI.FinanceTracker.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FinanceTrackerAPI.FinanceTracker.Domain.Entities;
 
 namespace FinanceTrackerAPI.FinanceTracker.API.Controllers
 {
@@ -10,6 +11,32 @@ namespace FinanceTrackerAPI.FinanceTracker.API.Controllers
     [Route("api/[controller]")]
     public class CategoryController : ControllerBase
     {
-        
+        private readonly ILogger<CategoryController> _logger;
+        private readonly FinanceTrackerDbContext _context;
+
+        public CategoryController(ILogger<CategoryController> logger, FinanceTrackerDbContext context) 
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("getAll")]
+        public async Task<IActionResult> GetAllCategories()
+        {
+            var categories = await _context.ExpenseCategories.ToListAsync();
+            return Ok(categories);
+        }
+
+        [HttpPost] 
+        [Route("createCategory")]
+
+        public async Task<IActionResult>CreateCategory(ExpenseCategory category)
+        {
+            await _context.ExpenseCategories.AddAsync(category); 
+            await _context.SaveChangesAsync();
+            return Ok(category);
+
+        }
     }
 }
