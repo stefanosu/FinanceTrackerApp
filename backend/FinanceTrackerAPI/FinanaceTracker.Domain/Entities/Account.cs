52dc106e0744e0eaf680f2e3e596579599a37d85
Stefanos Ugbit <stefanosu422@gmail.com>
2025-07-12 23:12:13 -0400

    fix: assign constructor parameters to Account properties correctly

--- backend/FinanceTrackerAPI/FinanaceTracker.Domain/Entities/Account.cs
+++ backend/FinanceTrackerAPI/FinanaceTracker.Domain/Entities/Account.cs
@@ -8,4 +8,15 @@ public class Account
     public required string Name { get; set; }
     public required string Email { get; set; }
     public required string PasswordHash { get; set; }
+
+
+    public Account(){}
+
+    public Account(int id, string name,  string email, string PasswordHash) 
+    {
+        this.id = id;
+        this.Name = name;
+        this.Email = email;
+        this.PasswordHash = PasswordHash;
+    }
 }
