11d3a8e1f5b38b239f5fceeaa9ff06f70d046feb
Stefanos Ugbit <stefanosu422@gmail.com>
2025-07-16 22:36:28 -0400

    added create account dto for post request account

---
+++ backend/FinanceTrackerAPI/Services/Dtos/CreateAccountDto.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinanceTrackerAPI.Services.Dtos
+{
+    namespace FinanceTrackerAPI.Services.Dtos
+    {
+        public class CreateAccountDto
+        {
+            // Add properties as needed, for example:
+            public string Name { get; set; }
+            public decimal InitialBalance { get; set; }
+        }
+    }
+}
